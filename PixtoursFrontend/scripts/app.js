9c93afd7c4e7502dbd1e333d01787f334c5d1dd8
gpastorino <gpastorino@gmail.com>
2020-03-09 16:43:05 -0700

    finalizes front-end template and starts on javascript hookups with Ajax call

--- PixtoursFrontend/scripts/app.js
+++ PixtoursFrontend/scripts/app.js
@@ -1,1 +1,14 @@
 console.log('Ground Control to Major Tom...');
+
+
+
+// function blogEntry(){
+//         console.log("clicked the blog add");
+//         document.getElementById("blogentry").onsubmit();
+// }
+
+
+
+const blogInput = document.getElementById('blogcontent');
+
+const 
